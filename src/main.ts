cfc4a55379314eb56c7e679dfed3a5d5f7e4771f
shun masuno <sh.masuno@gmail.com>
2025-12-30 17:56:49 +0900

    vuetify導入

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,31 @@ import outputs from "../amplify_outputs.json";
 import router from "./router";
 import { createPinia } from "pinia";
 
+// Vuetify //
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css"; // おそらくssrを使うとき必要
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 Amplify.configure(outputs);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
+app.use(vuetify);
 app.mount("#app");
